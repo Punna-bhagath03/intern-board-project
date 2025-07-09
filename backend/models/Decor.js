eb0494b258fb154e6b5566d7561612d8e84ff7b8
Punna-bhagath03 <punnabhagath03@gmail.com>
2025-07-09 13:33:21 +0530

    added decore section

---
+++ backend/models/Decor.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const DecorSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  originalFilename: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Decor', DecorSchema); 
